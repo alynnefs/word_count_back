9c23e28e642c1312833bb3eeff82297639eada01
Alynne Fereira <alynneferreiras@gmail.com>
2023-02-17 23:36:14 -0300

    correct test

--- tests/tests_count_the_words.py
+++ tests/tests_count_the_words.py
@@ -33,12 +33,12 @@ def test_multiple_spaces_between_words():
 
 def test_many_words():
     response = count_the_words(
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do"
-        + "eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut"
-        + "enim ad minim veniam, quis nostrud exercitation ullamco laboris"
-        + "nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in"
-        + "reprehenderit in voluptate velit esse cillum dolore eu fugiat"
-        + "nulla pariatur. Excepteur sint occaecat cupidatat non proident,"
-        + "sunt in culpa qui officia deserunt mollit anim id est laborum."
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed "
+        + "do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut "
+        + "enim ad minim veniam, quis nostrud exercitation ullamco "
+        + "laboris nisi ut aliquip ex ea commodo consequat. Duis aute "
+        + "irure dolor in reprehenderit in voluptate velit esse cillum "
+        + "dolore eu fugiat nulla pariatur."
     )
-    assert "63" == response
+
+    assert "52" == response
