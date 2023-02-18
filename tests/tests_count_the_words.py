0d7bb795d2ad13a6db9fc3adb82003724791d11a
Alynne Fereira <alynneferreiras@gmail.com>
2023-02-17 23:07:35 -0300

    add tests

---
+++ tests/tests_count_the_words.py
@@ -0,0 +1,44 @@
+from app.main import count_the_words
+
+
+def test_without_words():
+    response = count_the_words("")
+    assert "0" == response
+
+
+def test_only_with_space():
+    response = count_the_words(" ")
+    assert "0" == response
+
+
+def test_one_word():
+    response = count_the_words("apple")
+    assert "1" == response
+
+
+def test_two_words():
+    response = count_the_words("an apple")
+    assert "2" == response
+
+
+def test_space_start_and_end():
+    response = count_the_words(" an apple ")
+    assert "2" == response
+
+
+def test_multiple_spaces_between_words():
+    response = count_the_words("an    apple")
+    assert "2" == response
+
+
+def test_many_words():
+    response = count_the_words(
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do"
+        + "eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut"
+        + "enim ad minim veniam, quis nostrud exercitation ullamco laboris"
+        + "nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in"
+        + "reprehenderit in voluptate velit esse cillum dolore eu fugiat"
+        + "nulla pariatur. Excepteur sint occaecat cupidatat non proident,"
+        + "sunt in culpa qui officia deserunt mollit anim id est laborum."
+    )
+    assert "63" == response
